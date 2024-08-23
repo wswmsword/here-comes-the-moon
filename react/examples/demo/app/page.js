fe04846ac64ef3bd5b70e5d9ba378e0444c4e711
wswmsword <cjzjzzz@gmail.com>
2024-08-23 19:11:53 +0800

    add toggle

--- react/examples/demo/app/page.js
+++ react/examples/demo/app/page.js
@@ -6,14 +6,19 @@ import { useRef } from "react";
 export default function Home() {
   const moonRef = useRef();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24 gap-8">
       <Moon ref={moonRef} breakI={[0, 4]} />
-      <button onClick={() => {
-        moonRef.current.play();
-      }}>play</button>
-      <button onClick={() => {
-        moonRef.current.stop();
-      }}>stop</button>
+      <div style={{ display: "flex", gap: 9 }}>
+        <button onClick={() => {
+          moonRef.current.play();
+        }}>play</button>
+        <button onClick={() => {
+          moonRef.current.stop();
+        }}>stop</button>
+        <button onClick={() => {
+          moonRef.current.toggle();
+        }}>toggle</button>
+      </div>
     </main>
   );
 }
--- react/index.js
+++ react/index.js
@@ -1,6 +1,7 @@
-import React, { forwardRef, useImperativeHandle, useRef } from "react";
+import React, { forwardRef, useImperativeHandle, useRef, useState } from "react";
 
-export default forwardRef(function Moon({ defaultI = 0, breakI = [0, 4], moons = ["🌑", "🌘", "🌗", "🌖", "🌕", "🌔", "🌓", "🌒"], frameTm = 32, dur = 128, onEnd = () => {} }, ref) {
+export default forwardRef(function Moon({ defaultI = 0, breakI = [0, 4], moons = ["🌑", "🌘", "🌗", "🌖", "🌕", "🌔", "🌓", "🌒"], frameTm = 48, dur = 208, onEnd = () => {} }, ref) {
+  const [_moons, setM] = useState(moons);
   /** 当前月亮编号 */
   const curIRef = useRef(0);
   /** 元素月亮们 */
@@ -11,10 +12,11 @@ export default forwardRef(function Moon({ defaultI = 0, breakI = [0, 4], moons =
   const zIdRef = useRef(0);
 
   const _breakI = [].concat(breakI).filter(n => n != null);
+  const breakIRef = useRef(_breakI);
   /** 一共几帧画面 */
   const len = moons.length;
 
-  const spanMoons = moons.map((m, i) => {
+  const spanMoons = _moons.map((m, i) => {
     const isCur = defaultI === i;
     return <span
       onTransitionEnd={hidePrevFrame(i)}
@@ -31,13 +33,14 @@ export default forwardRef(function Moon({ defaultI = 0, breakI = [0, 4], moons =
     play,
     stop() {
       transformingRef.current = false;
-    }
+    },
+    toggle,
   }));
 
-  return <span style={{ position: "relative", transition: `visibility ${dur}ms, opacity ${dur}ms` }} role="presentation">
+  return <><span style={{ position: "relative", transition: `visibility ${dur}ms, opacity ${dur}ms` }} role="presentation">
     <span aria-hidden style={{ visibility: "hidden" }}>{moons[0]}</span>
     {spanMoons}
-  </span>;
+  </span></>;
 
   function hidePrevFrame(i) {
     return function(e) {
@@ -47,7 +50,7 @@ export default forwardRef(function Moon({ defaultI = 0, breakI = [0, 4], moons =
         moonRefs.current[prevI].style.zIndex = '';
         moonRefs.current[prevI].style.transition = '';
         moonRefs.current[prevI].style.opacity = 0;
-        if (_breakI.some(_i => _i === i) && curIRef.current === i) {
+        if (breakIRef.current.some(_i => _i === i) && curIRef.current === i) {
           transformingRef.current = false;
           onEnd();
         }
@@ -62,7 +65,6 @@ export default forwardRef(function Moon({ defaultI = 0, breakI = [0, 4], moons =
     transformingRef.current = true; // 上锁
 
     let prevTime = document.timeline.currentTime;
-    const len = moons.length;
 
     // 重置 z-index
     moonRefs.current[curIRef.current].style.zIndex = 0;
@@ -77,6 +79,7 @@ export default forwardRef(function Moon({ defaultI = 0, breakI = [0, 4], moons =
       if (timestamp - prevTime > frameTm) { // 间隔时间
 
         const curI = (curIRef.current + 1) % len;
+        // console.log(curI);
         curIRef.current = curI;
         zIdRef.current += 1;
         
@@ -87,10 +90,48 @@ export default forwardRef(function Moon({ defaultI = 0, breakI = [0, 4], moons =
         moonRefs.current[curI].style.transition = "inherit";
 
         // 新月和满月，结束
-        if (_breakI.some(i => i === curI)) return ;
+        if (breakIRef.current.some(i => i === curI)) return ;
       }
 
       window.requestAnimationFrame(moonFrame);
+    }
+  }
+
+  function toggle() {
+
+    if (!transformingRef.current) return play();
+    
+    const reversedMoons = _moons.toReversed();
+    const curI = curIRef.current;
+
+    if (len & 1 === 1 && (len - 1) / 2 === curI) {
+      _moons.current = reversedMoons
+      setM(_moons.current);
+      breakIRef.current = breakIRef.current.map(bI => (bI + len) % len);
+    } else {
+
+      const centerOffset = (curI + 1) > (len / 2) ? (curI + 1) * 2 - len : len - curI * 2;
+
+      const newMoons = [...reversedMoons];
+      const edgeOffset = (len - centerOffset + 2);
+
+      const times = Math.min(edgeOffset, centerOffset) - 1;
+
+      const isPop = ((curI < len / 2) && edgeOffset < centerOffset) || ((curI >= len / 2) && centerOffset < edgeOffset)
+
+      breakIRef.current = breakIRef.current.map(bI => isPop ? (bI + times - 1) % len : (bI - times + len - 1) % len);
+
+      for (let i = 0; i < times; ++ i) {
+
+        if (isPop) {
+          const moon = newMoons.pop();
+          newMoons.unshift(moon);
+        } else {
+          const moon = newMoons.shift();
+          newMoons.push(moon);
+        }
+      }
+      setM(newMoons);
     }
   }
 })
